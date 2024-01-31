80a47e6b605a71277d3ceda91ba49149a58ee5ad
Aaron Latham <latham91@icloud.com>
2024-01-31 16:01:46 +0000

    fixed errors

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,11 @@
 import { Link } from "react-router-dom";
 
-
 export default function NavBar() {
     return (
         <section id="navBar">
             <nav>
                 <Link to="/">Homepage</Link>
-                <Link to="//Cats/:id">CatPage</Link>
+                <Link to="/Cats/:id">CatPage</Link>
                 <Link to="/About">About</Link>
                 <Link to="/Checkout">Checkout</Link>
             </nav>
