b533690a866e376cd80ebac2dc7af379eb3c4c1f
Aaron Latham <latham91@icloud.com>
2024-01-31 14:42:08 +0000

    Added sliderHeader and SliderFooter

---
+++ src/components/BasketSlider.jsx
@@ -0,0 +1,36 @@
+import "./BasketSlider.css";
+
+import { FaXmark } from "react-icons/fa6";
+
+export default function BasketSlider() {
+    return (
+        <div className="sliderBackground">
+            <div className="sliderContent">
+                <div className="sliderHeader">
+                    <div className="sliderHeader_title">
+                        <h3>Basket</h3>
+                        <FaXmark size={28} className="sliderClose" />
+                    </div>
+                    <p>
+                        You have <strong>0 items</strong> in your basket.
+                    </p>
+                </div>
+                <div className="sliderItems">
+                    <div className="basketItem">
+                        <div className="basketItem_imageContainer">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="basketItem_content"></div>
+                    </div>
+                </div>
+                <div className="sliderFooter">
+                    <p className="sliderFooter_total">
+                        <strong>Total: </strong>
+                        <span>£0.00</span>
+                    </p>
+                    <button className="sliderFooter_button">Checkout</button>
+                </div>
+            </div>
+        </div>
+    );
+}
