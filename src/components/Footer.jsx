b763690053218f7a7ee001e909ef8e7317d76670
Tofeeq <tofeeq09@gmail.com>
2024-01-31 18:30:49 +0000

    styled navBar and edited the draw.io diagram & introduced a themes to index.css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
                                 <Link to="/">Home</Link>
                             </li>
                             <li>
-                                <Link to="/about">About Us</Link>
+                                <Link to="/about">About</Link>
                             </li>
                             <li>
                                 <Link to="/contact">Contact</Link>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,19 @@
 import { Link } from "react-router-dom";
+import  Logo from "../images/Logo.png"
+import "./NavBar.css";
 
 
 export default function NavBar() {
     return (
         <section id="navBar">
-            <nav className="logo">
-                <div>
+            <nav className="navbar-container">
+                <div className="logo">
                     <Link to="/">
                     <img src={Logo} alt="logo"/>
                     </Link>
                 </div>
                 <div className="pages">
-                    <Link to="/">Homepage</Link>
-                    <Link to="/Cats/:id">CatPage</Link>
+                    <Link to="/Cats/:id">Cat Page</Link>
                     <Link to="/About">About</Link>
                     <Link to="/Checkout">Checkout</Link>
                 </div>
