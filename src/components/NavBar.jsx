8f31d391e17bd14f7306e0125f04be9e0eb08211
Tofeeq <tofeeq09@gmail.com>
2024-01-31 16:36:30 +0000

    added logo to navBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,24 @@
 import { Link } from "react-router-dom";
-
+import Logo from "./images/Logo.jpg"
 
 export default function NavBar() {
     return (
         <section id="navBar">
-            <nav>
-                <Link to="/">Homepage</Link>
-                <Link to="//Cats/:id">CatPage</Link>
-                <Link to="/About">About</Link>
-                <Link to="/Checkout">Checkout</Link>
+            <nav className="logo">
+                <div>
+                    <Link to="/">
+                    <img src={Logo} alt="logo"/>
+                    </Link>
+                </div>
+                <div className="pages">
+                    <Link to="/">Homepage</Link>
+                    <Link to="/Cats/:id">CatPage</Link>
+                    <Link to="/About">About</Link>
+                    <Link to="/Checkout">Checkout</Link>
+                </div>
             </nav>
         </section>
     );
 }
+
+
