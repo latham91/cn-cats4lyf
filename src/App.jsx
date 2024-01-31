732852a231414d7f03653ff7b02d1b124dd9428e
Tofeeq <tofeeq09@gmail.com>
2024-01-31 14:41:43 +0000

    added navbar and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/Homepage";
+import About from "./pages/About"
+
 
 export default function App() {
     return (
         <>
             <Routes>
-                <Route path="/" element={<Homepage />} />
+                <Route path="/" element={<Homepage />}/>
+                <Route path="/Cats/:id" element={<CatPage />}/>
+                <Route path="/About" element={<About />}/>
+                <Route path="/Checkout" element={<CheckOut />}/>
             </Routes>
         </>
     );
 }
+
