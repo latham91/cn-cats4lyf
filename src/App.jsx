d68658d3f98926daf6c266ce727a95d431f9290b
Aaron Latham <latham91@icloud.com>
2024-01-31 16:16:39 +0000

    fixed error in about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/Homepage";
-import About from "./pages/About";
 import CatPage from "./pages/CatPage";
 import CheckOut from "./pages/CheckOut";
 import NavBar from "./components/NavBar";
+import AboutUs from "./pages/About";
 
 export default function App() {
     return (
@@ -13,7 +13,7 @@ export default function App() {
             <Routes>
                 <Route path="/" element={<Homepage />} />
                 <Route path="/Cats/:id" element={<CatPage />} />
-                <Route path="/About" element={<About />} />
+                <Route path="/About" element={<AboutUs />} />
                 <Route path="/Checkout" element={<CheckOut />} />
             </Routes>
         </>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,8 +7,8 @@ const AboutUs = () => {
                 providing the finest cats and creating a purrfect environment for them.
             </p>
             <p>
-                At Purradise Meowland, we believe in the magic of a cat's purr and the joy they bring to our lives. Our
-                mission is to connect cat enthusiasts with their dream feline companions.
+                At Purradise Meowland, we believe in the magic of a cat&apos;s purr and the joy they bring to our lives.
+                Our mission is to connect cat enthusiasts with their dream feline companions.
             </p>
             {/* Add more information about your company */}
         </div>
