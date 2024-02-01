94211a83fdbb18e1d4e58de656be59425f555862
Aaron Latham <latham91@icloud.com>
2024-02-01 15:47:50 +0000

    note

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,38 +1,35 @@
 import PropTypes from "prop-types";
 import "./Card.css";
 import { Link } from "react-router-dom";
-import { fakerData, descriptions } from "../utility/FakerData"
 
-export const Card = ({ imgSrc, link, breed }) => {
-
-    const catCardData = fakerData()
-
-    const description = descriptions[Math.floor(Math.random()*20)]
-    console.log(description)
-
+export const Card = ({ imgSrc, title, description, link }) => {
     return (
         <div className="card-container">
             <Link to={link}>
                 <div className="card-image-container">
-                    <img src={imgSrc} alt={catCardData.name + "image"} />
-                    <span className="card-breed">{breed}</span>
+                    <img src={imgSrc} alt={title + "image"} />
+                    <span className="card-breed">British Shorthair</span>
                 </div>
             </Link>
             <div className="card-content">
-                <h3>{catCardData.name}</h3>
+                <h3>{title}</h3>
                 <p>{description}</p>
 
                 <div className="card-actions">
                     <button>Add to Basket</button>
-                    <span>{catCardData.price}</span>
+                    <span>$99.00</span>
                 </div>
             </div>
         </div>
     );
 };
 
+Math.random();
+
 Card.propTypes = {
     imgSrc: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    buttonText: PropTypes.string,
     link: PropTypes.string,
-    breed: PropTypes.string,
 };
