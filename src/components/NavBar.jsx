d2557a7e6ee5223abe759216885ca6ac4e188918
Tofeeq <tofeeq09@gmail.com>
2024-02-01 10:44:41 +0000

    styled navBar and edited the draw.io diagram & introduced a themes to index.css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
 import Logo from "../images/Logo.png";
+import "./NavBar.css";
 
 export default function NavBar() {
     return (
         <section id="navBar">
-            <nav>
-                <div>
-                    <Link></Link>
+            <nav className="navbar-container">
+                <div className="logo">
+                    <Link to="/">
+                        <img src={Logo} alt="logo"/>
+                    </Link>
                 </div>
-                <div>
-                    <Link to="/">Homepage</Link>
+                <div className="pages">
                     <Link to="/Cats/:id">CatPage</Link>
                     <Link to="/About">About</Link>
                     <Link to="/Checkout">Checkout</Link>
