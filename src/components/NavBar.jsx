9a67a876b195e7198e8cd446a2cdfd5af8443598
Tofeeq <tofeeq09@gmail.com>
2024-02-01 11:41:17 +0000

    styled navBar more added a brand name to the nav bar, added a number next to the basket as a placeholder

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,28 @@
 import { Link } from "react-router-dom";
 import Logo from "../images/Logo.png";
 import "./NavBar.css";
+import { BsBasket } from "react-icons/bs";
+
 
 export default function NavBar() {
     return (
         <section id="navBar">
             <nav className="navbar-container">
+            <Link to="/">
                 <div className="logo">
-                    <Link to="/">
                         <img src={Logo} alt="logo"/>
-                    </Link>
+                    <h1>Cats4Lyf</h1>
                 </div>
+            </Link>
                 <div className="pages">
                     <Link to="/Cats/:id">CatPage</Link>
                     <Link to="/About">About</Link>
-                    <Link to="/Checkout">Checkout</Link>
+                    <Link to="/Checkout" className="cart-container">
+                        <div className="cart-container_basket">
+                                <BsBasket />
+                            <p>2</p>
+                        </div>
+                    </Link>
                 </div>
             </nav>
         </section>
