244d4b3148aa787a345246b48f86d03d4f215780
Aaron Latham <latham91@icloud.com>
2024-02-01 10:45:28 +0000

    added fetch function

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,11 @@
 import "./Homepage.css";
+import PropTypes from "prop-types";
 
-export default function Homepage() {
-    return (
-        <section id="homepage">
-            <div>Hello world</div>
-        </section>
-    );
+export default function Homepage({ cats, loading }) {
+    return <section id="homepage"></section>;
 }
+
+Homepage.propTypes = {
+    cats: PropTypes.array,
+    loading: PropTypes.bool,
+};
