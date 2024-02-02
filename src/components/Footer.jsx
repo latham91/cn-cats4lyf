d3e0b56e48ed60201b63e906af90f81d48b482a2
Oana <oanaloredana78@yahoo.co.uk>
2024-02-02 12:22:09 +0000

    note

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,25 +3,13 @@ const Footer = () => {
     <footer className="footer-container">
       <div className="container">
         <div className="row">
-          
+          {/* ... your existing code for the footer */}
         </div>
-
         <div className="row mt-4">
-          <div className="col-md-12">
+          <div className="col-md-12 footer-section">
             <h4 className="footer-heading">Contact Us</h4>
             <p className="footer-content">We're Here for You and Your Cats!</p>
-            <p className="footer-content">
-              Have a question, need assistance, or just want to talk about cats? We're all ears! Contact Cats for Life through the following channels:
-            </p>
-            <div>
-              <strong className="footer-content">Customer Support:</strong>
-              <ul>
-                <li className="footer-content">Email: <a href="mailto:support@cats4lyf.com">support@cats4lyf.com</a></li>
-                <li className="footer-content">Phone: [01612453651]</li>
-              </ul>
-              <strong className="footer-content">Visit Us:</strong>
-              <p className="footer-content">Cats for Life Headquarters<br />[2 Dale Street, Manchester, M3 5RD]</p>
-            </div>
+            {/* ... your existing code for the contact section */}
           </div>
         </div>
       </div>
@@ -30,11 +18,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
-
-
-
-
-
-
-
