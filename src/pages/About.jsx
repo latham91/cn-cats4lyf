3461e343ba370e6686929939864c333734b1023e
SAM101013 <amoonmarketing@proton.me>
2024-02-02 12:51:25 +0000

    aboutupdate

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,80 +1,91 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+// import Image1 from '..images/team.png';
+// import Image2 from "../images/welcome.png";
+
 
 
 const AboutUs = () => {
-    // Aaron Latham - Basket Functionality
-    // Ethan Rose - Card Component and Checkout Page
-    // Tofeeq Majeed - Navbar and Individual Cat Page
-    // Oana Lupascu - About and Footer
+    const containerStyle = {
+        padding: '20px', 
+        textAlign: 'center', 
+    };
 
     return (
         <footer className="bg-dark text-light">
-            <div className="container">
-                <div className="row">
+            <div className="container" style={containerStyle}> {}
+            <div className="col-md-12">
+                        {/* <img src={Image2} alt="Image2" />
+                        <img src={Image1} alt="Image1" /> */}
+
+                <h4>Cats4Lyf! We are here for you and your cats!</h4>
                     <div className="col-md-4">
-                        <h4> Cats for Life</h4>
-                        <p>We are here for you and your cats!</p>
-                        <p>
-                            Have a question, need assistance, or just want to talk about cats? We&apos;re all ears!
-                            Contact Cats for Life through the following channels:
-                        </p>
+                        
                         <div>
-                            <strong>Customer Support:</strong>
-                            <ul>
-                                <li>
-                                    Email: <a href="mailto:support@cats4lyf.com">support@cats4lyf.com</a>
-                                </li>
-                                <li>Phone: 01612453651</li>
-                            </ul>
-                            <strong>Visit Us:</strong>
-                            <p>
-                                Cats for Life Headquarters
-                                <br />2 Dale Street, Manchester, M3 5RD
-                            </p>
-                        </div>
+        
+            
+        </div>
+                        
+                        <p> Have a question, need assistance, or just want to talk about cats? </p>
+                        <p>We&apos;re all ears! Contact Cats for Life through the following channels:
+                        </p>
+                       
                     </div>
                     <div className="col-md-4">
-                        <h4>Connect With Us Online</h4>
+                        <h4>Connect With Us Online! Stay updated on the latest cat news!</h4>
                         <p>
-                            Stay updated on the latest cat news, promotions, and community events by following us on
+                             For promotions, and community events by following us on
                             social media:
                         </p>
+                        <strong>Customer Support: Phone: 01612453651</strong>
                         <ul>
-                            <li>
+                            <p>
                                 Facebook:{" "}
                                 <a href="https://facebook.com/cats4lyf" target="_blank" rel="noopener noreferrer">
                                     facebook.com/cats4lyf
                                 </a>
-                            </li>
-                            <li>
+                            </p>
+                            <p>
                                 Instagram:{" "}
                                 <a href="https://instagram.com/cats4lyf" target="_blank" rel="noopener noreferrer">
                                     instagram.com/cats4lyf
                                 </a>
-                            </li>
-                            <li>
+                            </p>
+                            <p>
                                 Twitter:{" "}
                                 <a href="https://twitter.com/cats4lyf" target="_blank" rel="noopener noreferrer">
                                     twitter.com/cats4lyf
                                 </a>
-                            </li>
+                            </p>
                         </ul>
+                        <div>
+                            
+                            <ul>
+                                <p>
+                                    Email: <a href="mailto:support@cats4lyf.com">support@cats4lyf.com</a>
+                                </p>
+                            </ul>
+                            <strong>Visit Us: Cats for Life Headquarters</strong>
+                            <p>
+                                <br />2 Dale Street, Manchester, M3 5RD
+                            </p>
+                        </div>
                         <p>
-                            <strong>Got Feedback?</strong> We value your feedback! Share your thoughts, suggestions, or
-                            experiences to help us improve and better serve you. Email us at{" "}
-                            <a href="mailto:feedback@cats4lyf.com">feedback@cats4lyf.com</a>.
+                            <strong>Got Feedback?</strong> We value your feedback! Share your thoughts to help us improve and better serve you. 
+                            <a href="mailto:feedback@cats4lyf.com">feedback@cats4lyf.com</a>
                         </p>
                     </div>
                     <div className="col-md-4">
-                        <h4>Business Inquiries</h4>
+                        
                         <p>
                             For business partnerships, collaborations, or wholesale inquiries, please reach out to our
-                            team at <a href="mailto:partnerships@cats4lyfe.com">partnerships@cats4lyfe.com</a>.
+                            team. <a href="">
+  
+</a>
+.
                         </p>
-                        <h4>Join Our Community</h4>
+                        <h4>Join Our Community: Become a part of the Cats for Life community by joining our forums.</h4>
                         <p>
-                            Become a part of the Cats for Life community by joining our forums, participating in events,
-                            and sharing your cat stories. We love hearing from fellow cat enthusiasts!
+                            Participate, by sharing your cat stories. We love hearing from fellow cat enthusiasts!
                         </p>
                         <h4>Operating Hours</h4>
                         <p>Our customer support team is available to assist you during the following hours:</p>
@@ -88,30 +99,6 @@ const AboutUs = () => {
                     </div>
                 </div>
 
-                <div className="row mt-4">
-                    <div className="col-md-12">
-                        <h4>Contact Us</h4>
-                        <p>We&apos;re Here for You and Your Cats!</p>
-                        <p>
-                            Have a question, need assistance, or just want to talk about cats? We&apos;re all ears!
-                            Contact Cats for Life through the following channels:
-                        </p>
-                        <div>
-                            <strong>Customer Support:</strong>
-                            <ul>
-                                <li>
-                                    Email: <a href="mailto:support@cats4lyf.com">support@cats4lyf.com</a>
-                                </li>
-                                <li>Phone: 01612453651</li>
-                            </ul>
-                            <strong>Visit Us:</strong>
-                            <p>
-                                Cats for Life Headquarters
-                                <br />2 Dale Street, Manchester, M3 5RD
-                            </p>
-                        </div>
-                    </div>
-                </div>
             </div>
         </footer>
     );
