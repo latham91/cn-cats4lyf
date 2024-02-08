fb6584235f318ad7e1d80c2b3f76d440cf4a59f4
Tofeeq <tofeeq09@gmail.com>
2024-02-08 18:18:51 +0000

    fix: catPage addToBasket tbn

--- src/pages/CatPage.jsx
+++ src/pages/CatPage.jsx
@@ -1,71 +1,89 @@
 import { Link, useParams } from "react-router-dom";
 import PropTypes from "prop-types";
-import "./Catpage.css";
+import "./CatPage.css";
 
-export default function CatPage({ catData }) {
-    const { id } = useParams(); // Get the id parameter from the url. eg: /Cats/1, 1 is the id.
-
-    // Find the cat from the catData state that matches the id from the url
-    const cat = catData.find((cat) => cat.id === id);
-
-    return (
-        <section id="cat-page">
-            {/* If the cat data is empty display a 404, cat not found
+export default function CatPage({ catData, handleAddToBasket }) {
+  const { id } = useParams(); // Get the id parameter from the url. eg: /Cats/1, 1 is the id.
+  // Find the cat from the catData state that matches the id from the url
+  const cat = catData.find((cat) => cat.id === id);
+  console.log(cat);
+  return (
+    <section id="cat-page">
+      {/* If the cat data is empty display a 404, cat not found
                 otherwise display the information from the cat data state.
             */}
-            {!cat ? (
-                <div className="errorPage">
-                    <h2>404: Cat Not Found!</h2>
-                    <div>
-                        <Link to="/">BACK TO HOME PAGE</Link>
-                    </div>
-                </div>
-            ) : (
-                <div className="cat-page-container">
-                    <div className="cat-page-left">
-                        <img src={cat.url} alt="cat image" />
-                    </div>
-                    <div className="cat-page-right">
-                        <div>
-                            <h2>{cat.name}</h2>
-                        </div>
-                        <div></div>
-                        <p>Breed: {cat.breed}</p>
-                        <p>Price: ${cat.price}</p>
-                        <p>
-                            Breed Info: <br />
-                            {cat.breedDescription}
-                        </p>
-                        <p>
-                            Breed Traits: <br />
-                            {cat.temperament}
-                        </p>
-                        <p>
-                            Origin of breed: <br />
-                            {cat.origin}
-                        </p>
-                        <p>
-                            Life expectancy: <br />
-                            {cat.life_span} years
-                        </p>
-                        <a href={cat.wikipedia_url}>Wiki Link</a>
-                        <p>
-                            Cat type: <br />
-                            {cat.indoor === 0 ? "Outdoor Cat" : "Indoor Cat"}
-                        </p>
-                        <p>
-                            Average weight: <br />
-                            {cat.weight}
-                        </p>
-                        <p>Rare: {cat.rare === 1 ? "Yes" : "No"}</p>
-                    </div>
-                </div>
-            )}
-        </section>
-    );
+      {!cat ? (
+        <div className="errorPage">
+          <h2>404: Cat Not Found!</h2>
+          <div>
+            <Link to="/">BACK TO HOME PAGE</Link>
+          </div>
+        </div>
+      ) : (
+        <div className="cat-page-container">
+          <div className="cat-page-left">
+            <img src={cat.url} alt="cat image" />
+          </div>
+          <div className="cat-page-right">
+            <div>
+              <h2>{cat.name}</h2>
+            </div>
+            <div></div>
+            <p>Breed: {cat.breed}</p>
+            <p>Price: ${cat.price}</p>
+            <p>
+              Breed Info: <br />
+              {cat.breedDescription}
+            </p>
+            <p>
+              Breed Traits: <br />
+              {cat.temperament}
+            </p>
+            <p>
+              Origin of breed: <br />
+              {cat.origin}
+            </p>
+            <p>
+              Life expectancy: <br />
+              {cat.life_span} years
+            </p>
+            <a href={cat.wikipedia_url}>Wiki Link</a>
+            <p>
+              Cat type: <br />
+              {cat.indoor === 0 ? "Outdoor Cat" : "Indoor Cat"}
+            </p>
+            <p>
+              Average weight: <br />
+              {cat.weight}
+            </p>
+            <p>Rare: {cat.rare === 1 ? "Yes" : "No"}</p>
+          </div>
+          <div className="card-actions">
+            {/* This function uses the addToBasket function to send back the
+                        id, name, price, breed and imageSrc to the parent component.
+                    */}
+            <button
+              onClick={() =>
+                handleAddToBasket(
+                  cat.id,
+                  cat.name,
+                  cat.price,
+                  cat.breed,
+                  cat.url
+                )
+              }
+            >
+              Add to Basket
+            </button>
+          </div>
+        </div>
+      )}
+    </section>
+  );
 }
 
 // These are prop types to ensure that the correct data types are passed to the component.
 CatPage.propTypes = {
-    catData: PropTypes.array,
+  catData: PropTypes.array,
+  handleAddToBasket: PropTypes.func,
 };
